e194165be1600840b3bb307a89830e801a28da34
Vincent Chu <c.tnecniv@gmail.com>
2025-12-16 02:50:57 +0800

    Fix remaining TypeScript errors in levenshtein function

--- frontend/src/composables/useRecipeDiff.ts
+++ frontend/src/composables/useRecipeDiff.ts
@@ -40,20 +40,20 @@ function levenshteinDistance(str1: string, str2: string): number {
   const n = str2.length
   const dp: number[][] = Array(m + 1).fill(null).map(() => Array(n + 1).fill(0))
 
-  for (let i = 0; i <= m; i++) dp[i][0] = i
-  for (let j = 0; j <= n; j++) dp[0][j] = j
+  for (let i = 0; i <= m; i++) dp[i]![0] = i
+  for (let j = 0; j <= n; j++) dp[0]![j] = j
 
   for (let i = 1; i <= m; i++) {
     for (let j = 1; j <= n; j++) {
       if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1]
+        dp[i]![j] = dp[i - 1]![j - 1]!
       } else {
-        dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+        dp[i]![j] = 1 + Math.min(dp[i - 1]![j]!, dp[i]![j - 1]!, dp[i - 1]![j - 1]!)
       }
     }
   }
 
-  return dp[m][n]
+  return dp[m]![n]!
 }
 
 /**
