5cbba4f7e81ee39c797b313aaf52e9f67d88cd4f
Vincent Chu <c.tnecniv@gmail.com>
2025-12-16 01:50:27 +0800

    Allow local storage fallback when S3 not configured

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # config.asset_host = "http://assets.example.com"
 
   # Store uploaded files on AWS S3 (see config/storage.yml for options).
-  config.active_storage.service = :amazon
+  config.active_storage.service = ENV['AWS_S3_BUCKET'].present? ? :amazon : :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
